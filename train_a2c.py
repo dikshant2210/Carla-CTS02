77afdc3931ca0ad4785b33b1cfe8357bcd2543f3
dikshant2210 <dikshant2210@gmail.com>
2021-10-25 00:23:25 +0200

    run simulator from script

--- train_a2c.py
+++ train_a2c.py
@@ -342,9 +342,9 @@ def run_server():
 
 
 if __name__ == '__main__':
-    # p = Process(target=run_server)
-    # p.start()
-    # time.sleep(5)  # wait for the server to start
+    p = Process(target=run_server)
+    p.start()
+    time.sleep(5)  # wait for the server to start
 
     main()
     # p.terminate()
