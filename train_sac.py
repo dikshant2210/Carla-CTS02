95a606036e135b1f2263c3ede577e523ec2fb794
Dikshant Gupta <dikshant2210@gmail.com>
2021-11-22 11:23:05 +0100

    training sac with new environment

--- train_sac.py
+++ train_sac.py
@@ -6,6 +6,7 @@ Time: 24.10.21 00:47
 import subprocess
 from multiprocessing import Process
 import numpy as np
+import os
 import random
 import pygame
 import carla
@@ -18,60 +19,29 @@ from torch.optim import Adam
 import torch.nn.functional as F
 
 from config import Config
+from environment.environment import GIDASBenchmark
 from agents.rl.sac.model import SAC, QNetwork
 from agents.rl.sac.utils import soft_update, hard_update, ExperienceBuffer
-from agents.navigation.rlagent import RLAgent
-from environment.world import World
-from environment.hud import HUD
-from agents.tools.scenario import Scenario
 
 
-def train_sac(args):
+def train_sac():
     ##############################################################
-    # Setting up simulator and world configuration #
-    pygame.init()
-    pygame.font.init()
+    t0 = time.time()
+    # Logging file
+    filename = "_out/sac/{}.log".format(datetime.now().strftime("%m%d%Y_%H%M%S"))
+    print(filename)
+    file = open(filename, "w")
+    file.write(str(vars(Config)) + "\n")
 
-    client = carla.Client(args.host, args.port)
-    client.set_timeout(60.0)
+    # Path to save model
+    path = "_out/sac/"
+    if not os.path.exists(path):
+        os.mkdir(path)
 
-    if args.display:
-        display = pygame.display.set_mode(
-            (args.width, args.height),
-            pygame.HWSURFACE | pygame.DOUBLEBUF)
-        display.fill((0, 0, 0))
-        pygame.display.flip()
+    # Setting up environment
+    env = GIDASBenchmark()
 
-    hud = HUD(args.width, args.height)
-    client.load_world('Town01')
-    wld = client.get_world()
-    settings = wld.get_settings()
-    settings.fixed_delta_seconds = Config.simulation_step
-    settings.synchronous_mode = Config.synchronous
-    wld.apply_settings(settings)
-
-    scene_generator = Scenario(wld)
-    scene = scene_generator.scenario01()
-    world = World(wld, hud, scene, args)
-    # controller = KeyboardControl(world)
-
-    wld_map = wld.get_map()
-    print(wld_map.name)
-    ##############################################################
-
-    ##############################################################
-    # Compiling all different combination of scenarios
-    episodes = list()
-    for scenario in Config.scenarios:
-        for speed in np.arange(Config.ped_speed_range[0], Config.ped_speed_range[1] + 1, 0.1):
-            for distance in np.arange(Config.ped_distance_range[0], Config.ped_distance_range[1] + 1, 1):
-                episodes.append((scenario, speed, distance))
-    random.shuffle(episodes)
-    ##############################################################
-
-    ##############################################################
-    # Instantiating all the symbolic and RL agent
-    planner_agent = RLAgent(world, wld.get_map(), scene)
+    # Instantiating RL agent
     torch.manual_seed(100)
     rl_agent = SAC(Config.num_actions).cuda()
     critic_optim = Adam(list(rl_agent.q_network.parameters()) + list(rl_agent.shared_network.parameters()), lr=args.lr)
@@ -79,9 +49,7 @@ def train_sac(args):
     hard_update(critic_target, rl_agent.q_network)
     policy_optim = Adam(list(rl_agent.action_policy.parameters()) + list(rl_agent.shared_network.parameters()),
                         lr=args.lr)
-    ##############################################################
 
-    ##############################################################
     # Setting up placeholders for SAC training
     my_buffer = ExperienceBuffer()
 
@@ -101,25 +69,22 @@ def train_sac(args):
 
     ##############################################################
     # Simulation loop
-    total_episodes = min(len(episodes) * 2, Config.num_episodes)
-    print("Total training episodes: {}".format(total_episodes))
-    for current_episode in range(total_episodes):
-        ##############################################################
-        # Get the scenario id, parameters and instantiate the world
-        idx = current_episode % len(episodes)
-        scenario_id, ped_speed, ped_distance = episodes[idx]
-        func = 'scene_generator.scenario' + scenario_id
-        scenario = eval(func + '()')
-        print("Episode: {}, Scenario: {}, Pedestrian Speed: {:.2f}m/s, "
-              "Ped_distance: {:.2f}m".format(current_episode + 1, scenario_id, ped_speed, ped_distance))
-        world.restart(scenario, ped_speed, ped_distance)
-        planner_agent.update_scenario(scenario)
+    current_episode = 0
+    max_episodes = Config.train_episodes
+    print("Total training episodes: {}".format(max_episodes))
+    file.write("Total training episodes: {}\n".format(max_episodes))
 
+    while current_episode < max_episodes:
+        m = env.reset()
+
+        # Setup placeholders for training value logs
         d = False
         rAll = 0
         j = 0
         episode_buffer = []
         total_episode_reward = 0
+
+        # Setup initial inputs for LSTM Cell
         cx = torch.zeros(1, 256).cuda().type(torch.cuda.FloatTensor)
         hx = torch.zeros(1, 256).cuda().type(torch.cuda.FloatTensor)
 
@@ -127,20 +92,17 @@ def train_sac(args):
         speed_action = 1
         velocity_x = 0
         velocity_y = 0
-        ##############################################################
+        nearmiss = False
+        accident = False
+        goal = False
 
-        ##############################################################
-        clock = pygame.time.Clock()
-        while j < Config.max_epLength:
-            clock.tick_busy_loop(60)
+        for step_num in range(Config.num_steps):
             if total_steps == Config.pre_train_steps:
                 print("Pre-train phase done.")
 
             j += 1
 
-            ##############################################################
             # Forward pass of the RL Agent
-            control, m = planner_agent.run_step()
             m = torch.from_numpy(m).cuda().type(torch.cuda.FloatTensor)
             cat_tensor = torch.from_numpy(np.array([reward, velocity_x, velocity_y, speed_action])).cuda().type(
                 torch.cuda.FloatTensor)
@@ -152,28 +114,16 @@ def train_sac(args):
 
             a = a.cpu().numpy()
             speed_action = np.argmax(a, axis=-1)[0]
-            if speed_action == 0:
-                control.throttle = 0.6
-            elif speed_action == 2:
-                control.throttle = -0.6
-            else:
-                control.throttle = 0
-            world.tick(clock)
-            if args.display:
-                world.render(display)
-                pygame.display.flip()
+            m1, r, done, info = env.step(speed_action)
 
-            if Config.synchronous:
-                frame_num = wld.tick()
-            world.player.apply_control(control)
-
-            _, m1 = planner_agent.run_step()
             m1 = torch.from_numpy(m1).cuda().type(torch.cuda.FloatTensor)
             m1 = torch.reshape(m1, (-1, 1, 100, 100))
-            velocity = planner_agent.vehicle.get_velocity()
+            velocity = info['velocity']
             velocity_x = velocity.x
             velocity_y = velocity.y
-            r, goal, accident, near_miss = planner_agent.get_reward()
+            goal = info['goal']
+            nearmiss = nearmiss or info['nearmiss']
+            accident = accident or info['accident']
             reward = r
             d = goal or accident
 
@@ -267,7 +217,7 @@ def train_sac(args):
             j_list.append(j)
             r_list.append(rAll)
 
-        print("Goal reached: {}, Near miss: {}, Crash: {}".format(goal, near_miss, accident))
+        print("Goal reached: {}, Near miss: {}, Crash: {}".format(goal, nearmiss, accident))
         if current_episode + 1 % Config.save_freq == 0:
             torch.save(rl_agent.state_dict(), "{}sac_{}.pth".format(Config.path, current_episode))
 
@@ -275,94 +225,42 @@ def train_sac(args):
         torch.save(rl_agent.state_dict(), "{}sac_{}.pth".format(Config.path, current_episode))
 
 
-def main():
-    parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
-    parser.add_argument(
-        '-v', '--verbose',
-        action='store_true',
-        dest='debug',
-        help='print debug information')
-    parser.add_argument(
-        '--host',
-        metavar='H',
-        default='127.0.0.1',
-        help='IP of the host server (default: 127.0.0.1)')
-    parser.add_argument(
+def main(args):
+    print(__doc__)
+
+    try:
+        train_sac()
+
+    except KeyboardInterrupt:
+        print('\nCancelled by user. Bye!')
+        pygame.quit()
+
+
+def run_server():
+    port = "-carla-port={}".format(Config.port)
+    subprocess.run(['cd /home/carla && SDL_VIDEODRIVER=offscreen ./CarlaUE4.sh -opengl ' + port], shell=True)
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(
+        description='CARLA Manual Control Client')
+    arg_parser.add_argument(
         '-p', '--port',
         metavar='P',
         default=2000,
         type=int,
         help='TCP port to listen to (default: 2000)')
-    parser.add_argument(
-        '-a', '--autopilot',
-        action='store_true',
-        help='enable autopilot')
-    parser.add_argument(
-        '--res',
-        metavar='WIDTHxHEIGHT',
-        default='1280x720',
-        help='window resolution (default: 1280x720)')
-    parser.add_argument(
-        '--filter',
-        metavar='PATTERN',
-        default='vehicle.audi.tt',
-        help='actor filter (default: "vehicle.audi.tt")')
-    parser.add_argument(
-        '--rolename',
-        metavar='NAME',
-        default='hero',
-        help='actor role name (default: "hero")')
-    parser.add_argument(
-        '--display',
-        default=False,
-        type=bool,
-        help='Render the simulation window (default: False)')
-    parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
-                        help='discount factor for reward (default: 0.99)')
-    parser.add_argument('--tau', type=float, default=0.005, metavar='G',
-                        help='target smoothing coefficient(τ) (default: 0.005)')
-    parser.add_argument('--lr', type=float, default=0.0003, metavar='G',
-                        help='learning rate (default: 0.0003)')
-    parser.add_argument('--alpha', type=float, default=0.2, metavar='G',
-                        help='Temperature parameter α determines the relative importance of the entropy\
-                                term against the reward (default: 0.2)')
-    parser.add_argument('--seed', type=int, default=123456, metavar='N',
-                        help='random seed (default: 123456)')
-    parser.add_argument('--batch_size', type=int, default=256, metavar='N',
-                        help='batch size (default: 256)')
-    parser.add_argument('--num_steps', type=int, default=1000001, metavar='N',
-                        help='maximum number of steps (default: 1000000)')
-    parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
-                        help='hidden size (default: 256)')
-    parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
-                        help='rl_agent updates per simulator step (default: 1)')
-    parser.add_argument('--start_steps', type=int, default=10000, metavar='N',
-                        help='Steps sampling random actions (default: 10000)')
-    parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
-                        help='size of replay buffer (default: 10000000)')
-    parser.add_argument('--cuda', action="store_true",
-                        help='run on CUDA (default: False)')
-    args = parser.parse_args()
+    arg_parser.add_argument(
+        '-ckp', '--checkpoint',
+        default='',
+        type=str,
+        help='load the model from this checkpoint')
+    arg = arg_parser.parse_args()
+    Config.port = arg.port
 
-    args.width, args.height = [int(x) for x in args.res.split('x')]
-
-    log_level = logging.DEBUG if args.debug else logging.INFO
-    logging.basicConfig(format='%(levelname)s: %(message)s', level=log_level)
-
-    logging.info('listening to server %s:%s', args.host, args.port)
-
-    train_sac(args)
-
-
-def run_server():
-    subprocess.run(['cd /home/carla && SDL_VIDEODRIVER=offscreen ./CarlaUE4.sh -opengl'], shell=True)
-    # subprocess.run(['cd ISDESPOT/isdespot-ped-pred/is-despot/problems/isdespotp_car/ && ./car'], shell=True)
-
-
-if __name__ == '__main__':
     p = Process(target=run_server)
     p.start()
     time.sleep(5)  # wait for the server to start
 
-    main()
-    # p.terminate()
+    main(arg)
+    p.terminate()
