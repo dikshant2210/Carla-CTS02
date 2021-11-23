29d904d437183eef333a3ea54994e41193556899
dikshant2210 <dikshant2210@gmail.com>
2021-11-23 12:58:15 +0100

    reduce batch size

--- config.py
+++ config.py
@@ -41,14 +41,13 @@ class Config:
     save_freq = 100
 
     # Setting the SAC training parameters
-    batch_size = 8  # 32  # How many experience traces to use for each training step.
+    batch_size = 4  # 32  # How many experience traces to use for each training step.
     trace_length = 8  # How long each experience trace will be when training
     update_freq = 200  # How often to perform a training step.
     y = .995  # Discount factor on the target Q-values
     startE = 1  # Starting chance of random action
     endE = 0.1  # Final chance of random action
     anneling_steps = 200000  # 10000 # How many steps of training to reduce startE to endE.
-    num_episodes = 100000  # How many episodes of game environment to train network with.
     pre_train_steps = 100  # 10000  # How many steps of random actions before training begins.
     load_model = True  # Whether to load a saved model.
     path = "_out/sac/"  # The path to save our model to.
