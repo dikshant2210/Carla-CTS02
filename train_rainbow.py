c0bb32be8048a22763464be8733c3ddc50613fac
dikshant2210 <dikshant2210@gmail.com>
2021-12-10 06:43:11 +0100

    args based batch size

--- train_rainbow.py
+++ train_rainbow.py
@@ -13,10 +13,10 @@ from Rainbow.rainbow import Rainbow
 from config import Config
 
 
-def train():
+def train(args):
     env = GIDASBenchmark()
     rainbow_trainer = Rainbow(env)
-    rainbow_trainer.train(batch_size=4)
+    rainbow_trainer.train(batch_size=args.batch_size)
 
 
 def run_server():
@@ -25,8 +25,18 @@ def run_server():
 
 
 if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(
+        description='CARLA Manual Control Client')
+    arg_parser.add_argument(
+        '-b', '--batch_size',
+        metavar='B',
+        default=16,
+        type=int,
+        help='Batch Size (default: 16)')
+    arg = arg_parser.parse_args()
+
     p = Process(target=run_server)
     p.start()
     time.sleep(5)
 
-    train()
+    train(arg)
