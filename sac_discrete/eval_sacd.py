1e29ae1ab0a2fb32f6fc5f43fbfcd91a796aa035
dikshant2210 <dikshant2210@gmail.com>
2021-12-20 17:08:14 +0100

    no display

--- sac_discrete/eval_sacd.py
+++ sac_discrete/eval_sacd.py
@@ -80,7 +80,7 @@ class EvalSacdAgent(BaseAgent):
             t[3 + 1] = 1.0  # index = 3 + last_action(maintain)
 
             while (not done) and episode_steps < self.max_episode_steps:
-                self.test_env.render()
+                # self.test_env.render()
 
                 start_time = time.time()
                 action = self.exploit((state, t))
