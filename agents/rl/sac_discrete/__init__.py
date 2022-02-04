40e479ce267c68ae55f4313c92040d75db7a4682
dikshant2210 <dikshant2210@gmail.com>
2022-02-05 00:51:16 +0100

    sac import bugs fix

--- agents/rl/sac_discrete/__init__.py
+++ agents/rl/sac_discrete/__init__.py
@@ -2,3 +2,8 @@
 Author: Dikshant Gupta
 Time: 07.12.21 11:55
 """
+
+from agents.rl.sac_discrete.sacd_agent import SacdAgent
+from agents.rl.sac_discrete.base import BaseAgent
+from agents.rl.sac_discrete.sacd.model import DQNBase, TwinnedQNetwork, CateoricalPolicy
+from agents.rl.sac_discrete.sacd.utils import disable_gradients
