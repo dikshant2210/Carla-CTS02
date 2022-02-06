5930c6731cca44eaedffd5a654bccde6c0eb4722
dikshant2210 <dikshant2210@gmail.com>
2022-02-06 19:30:08 +0100

    bug fix

--- agents/rl/sac_discrete/__init__.py
+++ agents/rl/sac_discrete/__init__.py
@@ -7,3 +7,4 @@ from agents.rl.sac_discrete.sacd_agent import SacdAgent
 from agents.rl.sac_discrete.base import BaseAgent
 from agents.rl.sac_discrete.sacd.model import DQNBase, TwinnedQNetwork, CateoricalPolicy
 from agents.rl.sac_discrete.sacd.utils import disable_gradients
+from agents.rl.sac_discrete.eval_sacd import EvalSacdAgent
