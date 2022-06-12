ffde44c0f8cf0b973d4c6f463dd3587b3e9d4dd4
dikshant2210 <dikshant2210@gmail.com>
2022-06-12 06:55:00 +0200

    more time to connect

--- hyleap/smart-car-sim-master/is-despot/problems/hybridVisual_car/src/connector.h
+++ hyleap/smart-car-sim-master/is-despot/problems/hybridVisual_car/src/connector.h
@@ -94,7 +94,7 @@ public:
             std::this_thread::sleep_for (std::chrono::seconds(1));
             tries++;
 
-            if(tries == 5){
+            if(tries == 25){
                 printf("\nConnection Failed: %s\n", strerror(errno));
                 exit(-1);
             }
