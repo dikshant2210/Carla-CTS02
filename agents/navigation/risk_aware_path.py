f990b9aa4d9f7274074623d67f0ca8e8c3e29eca
Dikshant Gupta <dikshant2210@gmail.com>
2022-06-30 16:54:21 +0200

    scenario 09 fix

--- agents/navigation/risk_aware_path.py
+++ agents/navigation/risk_aware_path.py
@@ -33,7 +33,9 @@ class PathPlanner:
         path.reverse()
         return path
 
-    def find_path_with_risk(self, start, end, costmap, obstacles, car_speed, yaw, risk_map, flag):
+    def find_path_with_risk(self, start, end, costmap, obstacles, car_speed, yaw, risk_map, flag, scenario):
+        if scenario == 9:
+            return self.find_path_with_risk_scenario09(start, end, costmap, obstacles, car_speed, yaw, risk_map, flag)
         try:
             path = self.find_path(start, end, costmap, obstacles, car_speed / 3.6, flag)
             if len(path):
@@ -43,6 +45,46 @@ class PathPlanner:
             else:
                 risk = np.inf
                 # TODO: DRF in this case
+        except:
+            path, risk = [], np.inf
+        return path, risk
+
+    def find_path_with_risk_scenario09(self, start, end, costmap, obstacles, car_speed, yaw, risk_map, flag):
+        checkpoint = (92, 14, -90)
+        try:
+            if start[1] <= checkpoint[1]:
+                if flag:
+                    paths = self.anytime_planner.find_path(start, end, costmap, obstacles, car_speed, weight=0.9)
+                else:
+                    paths = self.path_planner.find_path(start, end, costmap, obstacles)
+                if len(paths):
+                    path = paths[0]
+                else:
+                    path = []
+                path.reverse()
+
+            else:
+                if flag:
+                    path_segemnt_1 = self.anytime_planner.find_path(start, checkpoint, costmap, obstacles,
+                                                                    car_speed, weight=0.9)[0]
+                    path_segemnt_2 = self.anytime_planner.find_path(checkpoint, end, costmap, obstacles,
+                                                                    car_speed, weight=0.9)[0]
+                    path_segemnt_2.reverse()
+                    path_segemnt_1.reverse()
+                    path = path_segemnt_1[:-1] + path_segemnt_2[1:]
+                else:
+                    path_segemnt_1 = self.path_planner.find_path(start, checkpoint, costmap, obstacles)[0]
+                    path_segemnt_2 = self.path_planner.find_path(checkpoint, end, costmap, obstacles)[0]
+                    path_segemnt_2.reverse()
+                    path_segemnt_1.reverse()
+                    path = path_segemnt_1[:-1] + path_segemnt_2[1:]
+
+            if len(path):
+                player = [start[0], start[1], car_speed, yaw]
+                steering_angle = path[2][2] - start[2]
+                risk, drf = self.risk_estimator.get_risk(player, steering_angle, risk_map)
+            else:
+                risk = np.inf
         except:
             path, risk = [], np.inf
         return path, risk
