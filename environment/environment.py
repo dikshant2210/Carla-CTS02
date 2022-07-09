21e90e528128d497d06100ec889453e52758cbc9
Dikshant Gupta <dikshant2210@gmail.com>
2022-07-09 08:54:24 +0200

    check

--- environment/environment.py
+++ environment/environment.py
@@ -122,6 +122,7 @@ class GIDASBenchmark(gym.Env):
         # if speed > 20:
         #     action = 2
         if self.scenario == '10':
+            print("hola!")
             if speed < 20:
                 action = 0
             elif speed > 50:
